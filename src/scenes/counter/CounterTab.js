caa13fb1bccbaee59a3f1858b7eaf8833eedf747
Wee Kit <wjz1012.1999@gmail.com>
2021-08-01 08:29:48 +0800

    Sign out feature complete

--- src/scenes/counter/CounterTab.js
+++ src/scenes/counter/CounterTab.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { useToast } from "react-native-styled-toast";
 
 import global from "_scenes/login/Global";
@@ -13,14 +14,46 @@ const CounterTab = ({ navigation }) => {
     toast({ message: "Login successfully" });
   }, []);
 
+  const showLogOutAlert = () => {
+    Alert.alert(
+      "Alert",
+      "Are you sure you wanna sign out?",
+      [
+        { text: "No Thanks", onPress: () => {} },
+        { text: "Yes", onPress: () => navigation.navigate("Login") },
+      ],
+      {
+        cancelable: true,
+      }
+    );
+  };
+
   return (
     <View
       style={[
         sty.container,
-        { justifyContent: "center", alignItems: "center" },
+        // { justifyContent: "center", alignItems: "center" },
       ]}
     >
-      <Text>This is counter tab</Text>
+      <View style={{ flex: 1, marginTop: 10, backgroundColor: "transparent" }}>
+        <Text style={sty.titleText}>
+          Dashboard <Ionicons name="ios-menu" size={24} color="black" />
+        </Text>
+        <View style={sty.titleDivider} />
+      </View>
+      <View style={{ flex: 9, alignItems: "center" }}>
+        <TouchableOpacity
+          style={localStyle.button}
+          onPress={() => {
+            navigation.navigate("MenuListScreen");
+          }}
+        >
+          <Text style={localStyle.buttonText}>Menu</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={localStyle.button} onPress={showLogOutAlert}>
+          <Text style={localStyle.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -31,6 +64,33 @@ const localStyle = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  button: {
+    height: 60,
+    width: 250,
+    backgroundColor: "#f98640",
+    paddingHorizontal: 0,
+    paddingVertical: 0,
+    borderRadius: 40,
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontFamily: "inter-bold",
+    color: "#fff",
+    justifyContent: "center",
+    fontSize: 20,
+  },
+  // titleText: {
+  //   fontFamily: "inter-bold",
+  //   fontSize: 30,
+  // },
+  // titleDivider: {
+  //   marginTop: 5,
+  //   height: 1,
+  //   backgroundColor: "black",
+  //   opacity: 0.2,
+  // },
 });
 
 export default CounterTab;
